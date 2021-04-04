a64294f711794554b571100732ba592bfe38f74a
Ali Hammoud <alloochi.hammoud@gmail.com>
2021-04-04 23:01:32 +0300

    Added search bar

--- src/components/QuestionsComponent.js
+++ src/components/QuestionsComponent.js
@@ -1,4 +1,4 @@
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Label } from 'reactstrap';
 import { Control, LocalForm } from 'react-redux-form';
 import  Button  from '@material-ui/core/Button'
 import React from 'react';
@@ -8,6 +8,14 @@ const QuestionsComponent = () => {
     return (
         <React.Fragment>
             <Button fullWidth startIcon={<Add />} variant="contained" size="large">Ask a Question</Button>
+            <LocalForm className="form">
+                <Row className="form-group">
+                    <Col md={12}>
+                        <Control.text model=".search" id="search" name="search" 
+                        placeholder="Type to Search.." className="form-control" />
+                    </Col>
+                </Row>
+            </LocalForm>
         </React.Fragment>
     )
 }
