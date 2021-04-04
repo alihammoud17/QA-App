a793fce9ad67880cff089d9a2dd12990d379b7e4
Ali Hammoud <alloochi.hammoud@gmail.com>
2021-04-04 22:32:38 +0300

    Adding Header and Button

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react'
+
+const HeaderComponent = ({title}) => {
+    return (
+    <div className='header'>
+      <h1>{title}</h1>
+    </div>
+    )
+}
+
+export default HeaderComponent
---
+++ src/components/QuestionsComponent.js
@@ -0,0 +1,15 @@
+import { Row, Col } from 'reactstrap';
+import { Control, LocalForm } from 'react-redux-form';
+import  Button  from '@material-ui/core/Button'
+import React from 'react';
+import { Add } from '@material-ui/icons';
+
+const QuestionsComponent = () => {
+    return (
+        <React.Fragment>
+            <Button fullWidth startIcon={<Add />} variant="contained" size="large">Ask a Question</Button>
+        </React.Fragment>
+    )
+}
+
+export default QuestionsComponent
