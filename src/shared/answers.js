55e98aa108548720d3904ac8779097c2f8651a3c
Ali Hammoud <alloochi.hammoud@gmail.com>
2021-04-05 21:53:27 +0300

    Answers configured for test

---
+++ src/shared/answers.js
@@ -0,0 +1,100 @@
+export const ANSWERS = [
+    {
+        id: 0,
+        qId: 0,
+        author: 'Ali Hammoud',
+        answer: '1978 and 1982',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 1,
+        qId: 0,
+        author: 'Ali Hammoud',
+        answer: 'Argentina won it in 1978 and 1982',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 2,
+        qId: 0,
+        author: 'Ali Hammoud',
+        answer: 'Argentina won the world cup in 1978 and 1982.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 3,
+        qId: 1,
+        author: 'Ali Hammoud',
+        answer: 'A hamburger contains 250 calories in average.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 4,
+        qId: 1,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 5,
+        qId: 2,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 6,
+        qId: 2,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 7,
+        qId: 2,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 8,
+        qId: 2,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 9,
+        qId: 3,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 10,
+        qId: 4,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 11,
+        qId: 5,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 12,
+        qId: 6,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 13,
+        qId: 7,
+        author: 'Ali Hammoud',
+        answer: 'It depends on its constituants, but in average its around 250 kcal.',
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+]
