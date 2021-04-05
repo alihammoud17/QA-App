3717a156ec43bb679eac77ffa033e9cd1f6efec4
Ali Hammoud <alloochi.hammoud@gmail.com>
2021-04-06 01:50:55 +0300

    Debugged Render Question

--- src/components/PostQuestions.js
+++ src/components/PostQuestions.js
@@ -3,28 +3,31 @@ import Loader from 'react-loader-spinner';
 import { Card, CardContent, CardActionArea } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { Row } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 const RenderQuestion = ({question}) => {
     return (
         <Card variant="outlined">
-        <CardActionArea>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {question.question}
-                    </Typography>
-                    <Typography variant="body1" color="textPrimary">
-                        {question.category}
-                    </Typography>
-                    <Typography variant="body2" color="text-secondary">
-                       asked by {question.author}, on {new Intl.DateTimeFormat('en-US',{
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(question.date))}
-                    </Typography>
-                </CardContent>
-        </CardActionArea>
-    </Card>
+            <Link to={`/question/${question.id}`}>
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {question.question}
+                        </Typography>
+                        <Typography variant="body1" color="textPrimary">
+                            {question.category}
+                        </Typography>
+                        <Typography variant="body2" color="text-secondary">
+                        asked by {question.author}, on {new Intl.DateTimeFormat('en-US',{
+                            year: 'numeric',
+                            month: 'long',
+                            day: '2-digit'
+                        }).format(new Date(question.date))}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Link>
+        </Card>
     );
     
 }
